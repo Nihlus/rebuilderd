521b642c6055985c7d00134ebe89f72e8dcd01d8
Jarl Gullberg <jarl.gullberg@gmail.com>
2025-06-16 21:08:06 +0200

    Enable zstd compression for outgoing requests and incoming data on the client side.

--- common/src/api/v1/mod.rs
+++ common/src/api/v1/mod.rs
@@ -1,6 +1,6 @@
 mod models;
 
-use crate::api::Client;
+use crate::api::{Client, ZstdRequestBuilder};
 use crate::errors::*;
 use async_trait::async_trait;
 pub use models::*;
@@ -132,7 +132,7 @@ impl BuildRestApi for Client {
     async fn submit_build_report(&self, request: RebuildReport) -> Result<()> {
         self.post(Cow::Borrowed("api/v1/builds"))
             .json(&request)
-            .send()
+            .send_encoded()
             .await?
             .error_for_status()?;
 
@@ -351,7 +351,7 @@ impl PackageRestApi for Client {
     async fn submit_package_report(&self, report: &PackageReport) -> Result<()> {
         self.post(Cow::Borrowed("api/v1/packages"))
             .json(report)
-            .send()
+            .send_encoded()
             .await?
             .error_for_status()?;
 
@@ -448,7 +448,7 @@ impl QueueRestApi for Client {
     async fn request_rebuild(&self, request: QueueJobRequest) -> Result<()> {
         self.post(Cow::Borrowed("api/v1/queue"))
             .json(&request)
-            .send()
+            .send_encoded()
             .await?
             .error_for_status()?;
 
@@ -495,7 +495,7 @@ impl QueueRestApi for Client {
         let record = self
             .post(Cow::Borrowed("api/v1/queue/pop"))
             .json(&request)
-            .send()
+            .send_encoded()
             .await?
             .error_for_status()?
             .json()
@@ -532,7 +532,7 @@ impl WorkerRestApi for Client {
     async fn register_worker(&self, request: RegisterWorkerRequest) -> Result<()> {
         self.post(Cow::Borrowed("api/v1/workers"))
             .json(&request)
-            .send()
+            .send_encoded()
             .await?
             .error_for_status()?;
 
